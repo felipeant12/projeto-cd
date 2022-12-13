b7f193f5f17d825d8e197cc7a81ffb0bf70f574e
felipe <felipee_12ant@hotmail.com>
2022-12-12 23:55:52 -0300

    primeira pagina

---
+++ pages/index.js
@@ -0,0 +1,27 @@
+import react from "react"
+import { useState } from 'react' ;
+function Home() {
+    return (
+    
+    <div>
+        <h2>Home 3</h2>
+    <Contador />
+    </div>
+    )
+}
+
+function Contador() {
+    const [contador, setContador] = useState(1);
+
+    function adicionarContador() {
+        setContador(contador +1);
+    }
+    return (
+    <div>
+        <div>{contador}</div>
+        <button onClick={adicionarContador}>ADICIONAR</button>
+    </div>
+)
+    
+}
+export default Home
